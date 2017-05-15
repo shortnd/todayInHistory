4e23290ddabb051d40cf2440b7fce0c17fcd63c6
Collin OConnell <ococncol@gmail.com>
2017-05-15 10:12:42 -0400

    setup js folder and style folder

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php  ?>
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Today in History</title>
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/script.js" charset="utf-8"></script>
+  </body>
+</html>
