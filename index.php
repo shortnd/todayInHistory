c530cfde098f0a9276f39a6d8ea33c5abd5a9058
Collin OConnell <ococncol@gmail.com>
2017-05-15 11:32:20 -0400

    was able to get the data that I need to display   now working on displaying one at a time

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
+// $date = date('M d Y');
+
+$todayPage = file_get_contents('https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/today');
+
+$todayDate = explode('<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr"><p><b>', $todayPage);
+
+$endTodayDate = explode('<div class="thumbinner mp-thumb" style="background: transparent; border: none; padding: 0;', $todayDate[1]);
+
+$todayListOne = explode('</span></i></div>
+</div>
+</div>
+<ul>', $todayPage);
+
+$todayListOneEnd = explode('</ul>
+<p><b><a href="', $todayListOne[1]);
+
+// $todayListTwo = explode('</li>
+// <li>', $todayPage);
+//
+// $todayListThree = explode('</li>
+// <li>', $todayPage);
 
 
 ?>
@@ -10,9 +31,28 @@
     <title>Today in History</title>
   </head>
   <body>
-    <h1>Hello World!</h1>
+    <main>
+      <header>
+        <h1>Today in History</h1>
+        <h2><?php echo $date ?></h2>
+      </header>
 
+      <section>
+        <?php
+        echo $endTodayDate[0];
+        ?>
 
+        <p><?php
+        echo $todayListOneEnd[0];
+
+        // echo $todayListTwo[1];
+      ?><p>
+      </section>
+
+      <footer>
+
+      </footer>
+    </main>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/script.js" charset="utf-8"></script>
   </body>
